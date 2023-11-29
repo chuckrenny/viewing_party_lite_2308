841fdd695d4e781fa4dccf1d6136d441a2d1ab74
Taylor Pubins <tpubz@icloud.com>
2023-11-28 20:58:16 -0700

    Add user story

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe "Discover Movies", type: :feature do
     @user = create :user
   end
 
+  # As a user,
+  # When I go to a user dashboard,
+  # and click "Discover Movies" button,
+  # I am redirected to a discover page /users/:id/discover,
+  # where :id is the user id of the user who's dashboard I was just on.
   describe "redirect action from user dashboard" do
     it "routes from user dashboard to the discover movies page" do
       visit user_path(@user)
